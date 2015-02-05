3711a31e984a2dd1208cdba2fb064581f826f90f
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2015-02-05 18:19:39 -0500

    Add resolved (unstable?) url for chef-server-core deb. Should prob enforce checksum.

--- cookbooks/bcpc/files/default/build_bins.sh
+++ cookbooks/bcpc/files/default/build_bins.sh
@@ -105,11 +105,12 @@ if [ -z `gem list --local fpm | grep fpm | cut -f1 -d" "` ]; then
 fi
 
 # Fetch chef client and server debs
-#CHEF_CLIENT_URL=https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/13.04/x86_64/chef_10.32.2-3_amd64.deb
-#CHEF_CLIENT_URL=https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef_11.10.4-1.ubuntu.12.04_amd64.deb
-CHEF_CLIENT_URL=https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/13.04/x86_64/chef_12.0.0-rc.0-1_amd64.deb
-CHEF_CLIENT_BOOTSTRAP_URL=https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef_10.32.2-1_amd64.deb
-CHEF_SERVER_URL=https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef-server_11.0.12-1.ubuntu.12.04_amd64.deb
+CHEF_CLIENT_URL=https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/13.04/x86_64/chef_12.0.3-1_amd64.deb
+#CHEF_CLIENT_BOOTSTRAP_URL=https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef_10.32.2-1_amd64.deb
+CHEF_CLIENT_BOOTSTRAP_URL=$CHEF_CLIENT_URL
+#CHEF_SERVER_URL=https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef-server_11.0.12-1.ubuntu.12.04_amd64.deb
+#TODO: maybe unstable url...?
+CHEF_SERVER_URL=https://packagecloud.io/chef/stable/ubuntu/pool/trusty/main/c/chef-server-core/chef-server-core_12.0.3-1_amd64.deb
 
 if [ ! -f chef-client.deb ]; then
    ccurl  ${CHEF_CLIENT_URL} chef-client.deb
