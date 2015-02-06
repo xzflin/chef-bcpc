2924598aad5d42bb65af78de706d4746b61f7f3f
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2015-02-06 16:13:10 -0500

    Seems to create the user accounts and install chef as per docs.

--- setup_chef_server.sh
+++ setup_chef_server.sh
@@ -22,37 +22,36 @@ if [[ -z "$CURL" ]]; then
 	exit
 fi
 
-if dpkg -s chef-server 2>/dev/null | grep -q Status.*installed; then
-  echo chef-server is installed
+if dpkg -s chef-server-core 2>/dev/null | grep -q Status.*installed; then
+  echo chef server is installed
 else
   dpkg -i cookbooks/bcpc/files/default/bins/chef-server.deb
-  if [ ! -f /etc/chef-server/chef-server.rb ]; then
-    if [ ! -d /etc/chef-server ]; then
-      mkdir /etc/chef-server
-      chown 775 /etc/chef-server
-    fi
-    cat > /etc/chef-server/chef-server.rb <<EOF
-api_fqdn "${BOOTSTRAP_IP}"
-# allow connecting to http port directly
-nginx['enable_non_ssl'] = true
-# have nginx listen on port 4000
-nginx['non_ssl_port'] = 4000
-# allow long-running recipes not to die with an error due to auth
-erchef['s3_url_ttl'] = 3600
-EOF
+#  if [ ! -f /etc/chef/chef-server.rb ]; then
+#    if [ ! -d /etc/chef ]; then
+#      mkdir /etc/chef
+#      chown 775 /etc/chef
+#    fi
+#    cat > /etc/chef/chef-server.rb <<EOF
+#api_fqdn "${BOOTSTRAP_IP}"
+## allow connecting to http port directly
+#nginx['enable_non_ssl'] = true
+## have nginx listen on port 4000
+#nginx['non_ssl_port'] = 4000
+## allow long-running recipes not to die with an error due to auth
+#erchef['s3_url_ttl'] = 3600
+#EOF
+#  fi
+  chef-server-ctl reconfigure
+  # TODO: change the names? idk
+  if [[ -n $SUDO_USER ]]; then
+    OWNER=$SUDO_USER
+  else
+    OWNER=$USER
   fi
-  sudo chef-server-ctl reconfigure
+  install -d -m0700 -o $OWNER $HOME/.knife
+  chef-server-ctl user-create admin admin admin admin@localhost.com welcome --filename $HOME/.knife/admin.pem
+  chef-server-ctl org-create bcpc "BCPC" --association admin --filename $HOME/.knife/chef-validator.pem
 fi
-
-if dpkg -s chef 2>/dev/null | grep -q Status.*installed; then
-  echo chef is installed
-else
-  dpkg -i cookbooks/bcpc/files/default/bins/chef-client-bootstrap.deb
-fi
-
-chmod +r /etc/chef-server/admin.pem
-chmod +r /etc/chef-server/chef-validator.pem
-chmod +r /etc/chef-server/chef-webui.pem
 
 # copy our ssh-key to be authorized for root
 if [[ -f $HOME/.ssh/authorized_keys && ! -f /root/.ssh/authorized_keys ]]; then
