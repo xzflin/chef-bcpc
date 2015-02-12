e9df1d4c5958843ab4dc7754bebc77531c479d0a
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2015-02-12 16:22:34 -0500

    Add cert hack for MITM proxy certs.

---
+++ proxy_cert_download_hack.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# This should not be considered a good idea in any measure...
+# Also script assumes root privs
+hash -r
+set -e
+
+port=8443   # what if already occupied?
+target=supermarket.chef.io
+cafile=/opt/chef/embedded/ssl/cert.pem
+
+if [[ -f ./proxy_setup.sh ]]; then
+  . ./proxy_setup.sh
+fi
+
+apt-get install proxytunnel
+
+proxytunnel -p ${https_proxy##*://} -d $target:443 -a ${port} &
+pid=$!
+trap 'kill ${pid}' HUP EXIT INT
+sleep 1
+#TODO: check for some sort of failure?
+cp -H "${cafile}" "${cafile}.old"
+echo | openssl s_client -connect 127.0.0.1:$port -showcerts | sed -n '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p' >> "${cafile}"
