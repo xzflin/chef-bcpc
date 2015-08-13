97d32735906fce980e5f76fdb02c832bd6c1a80d
Edmund Rhudy <erhudy@users.noreply.github.com>
2015-08-13 17:11:06 -0400

    add bcpc-binary-files cookbook to wrap binaries in a single location

---
+++ cookbooks/bcpc-binary-files/README.md
@@ -0,0 +1,6 @@
+bcpc-binary-files Cookbook
+==========================
+
+Files that go in this cookbook
+---
+* ubuntu-14.04-mini.iso
---
+++ cookbooks/bcpc-binary-files/metadata.rb
@@ -0,0 +1,7 @@
+name             'bcpc-binary-files'
+maintainer       'Bloomberg Finance L.P.'
+maintainer_email 'bcpc@bloomberg.net'
+license          'Apache 2.0'
+description      'This cookbook has no recipes and serves only to encapsulate all binary files used by BCPC cookbooks.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '6.0.0'
