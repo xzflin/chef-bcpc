cd893c4af2d3bdc97049274d3c0868e746a2cea5
Edmund Rhudy <erhudy@users.noreply.github.com>
2015-08-14 19:40:31 -0400

    health checks converge on headnode, in progress (moving things around)

--- cookbooks/bcpc-health-check/recipes/default.rb
+++ cookbooks/bcpc-health-check/recipes/default.rb
@@ -16,3 +16,43 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+directory "/usr/local/bin/checks" do
+  action :create
+  owner "root"
+  group "root"
+  mode 00775
+end
+
+directory "/usr/local/etc/checks" do
+  action :create
+  owner "root"
+  group "root"
+  mode 00775
+end
+
+ template  "/usr/local/etc/checks/default.yml" do
+   source "default.yml.erb"
+   owner "root"
+   group "root"
+   mode 00640
+ end
+
+ cookbook_file "/usr/local/bin/check" do
+   source "check"
+   owner "root"
+   mode "00755"
+ end
+
+template  "/usr/local/etc/checks/nova.yml" do
+  source "nova.yml.erb"
+  owner "root"
+  group "root"
+  mode 00640
+end
+
+cookbook_file "/usr/local/bin/checks/nova" do
+  source "nova"
+  owner "root"
+  mode "00755"
+end
---
+++ cookbooks/bcpc-health-check/recipes/headnode.rb
@@ -0,0 +1,46 @@
+#
+# Cookbook Name:: bcpc-health-check
+# Recipe:: headnode
+#
+# Copyright 2015, Bloomberg Finance L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe 'bcpc-health-check'
+
+%w{ mysql apache }.each do |cc|
+  template  "/usr/local/etc/checks/#{cc}.yml" do
+    source "#{cc}.yml.erb"
+    owner "root"
+    group "root"
+    mode 00640
+  end
+
+  cookbook_file "/usr/local/bin/checks/#{cc}" do
+    source "#{cc}"
+    owner "root"
+    mode "00755"
+  end
+
+  # this requires the Zabbix agent to be installed
+  if node['bcpc']['enabled']['monitoring']
+    cron "check-#{cc}" do
+      home "/var/lib/zabbix"
+      user "root"
+      minute "*/10"
+      path "/usr/local/bin:/usr/bin:/bin"
+      command "zabbix_sender -c /etc/zabbix/zabbix_agentd.conf --key 'check.#{cc}' --value `check -f timeonly #{cc}` 2>&1 | /usr/bin/logger -p local0.notice"
+    end
+  end
+end
