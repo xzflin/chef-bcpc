45b64d9e94efe7760a178aba711672c3dce7d719
Edmund Rhudy <erhudy@users.noreply.github.com>
2015-08-14 14:39:54 -0400

    keepalived headnode converges

--- cookbooks/bcpc-keepalived/recipes/default.rb
+++ cookbooks/bcpc-keepalived/recipes/default.rb
@@ -16,3 +16,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+package "keepalived" do
+    action :upgrade
+end
+
+service "keepalived" do
+    action [:enable, :start]
+end
