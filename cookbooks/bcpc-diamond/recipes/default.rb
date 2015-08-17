40d0011b4bd4e45bc53da35f8282e43a0008c1a0
Edmund Rhudy <erhudy@users.noreply.github.com>
2015-08-17 10:54:29 -0400

    bcpc-diamond converges

--- cookbooks/bcpc-diamond/recipes/default.rb
+++ cookbooks/bcpc-diamond/recipes/default.rb
@@ -16,3 +16,70 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+if node['bcpc']['enabled']['metrics']
+    cookbook_file "/tmp/diamond.deb" do
+        source "diamond.deb"
+        cookbook "bcpc-binary-files"
+        owner "root"
+        mode 00444
+    end
+
+    %w{python-support python-configobj python-pip python-httplib2}.each do |pkg|
+        package pkg do
+            action :upgrade
+        end
+    end
+
+    package "diamond" do
+        provider Chef::Provider::Package::Dpkg
+        source "/tmp/diamond.deb"
+        action :upgrade
+        options "--force-confold --force-confdef"
+    end
+
+    if node['bcpc']['virt_type'] == "kvm"
+        package "ipmitool" do
+            action :upgrade
+        end
+        package "smartmontools" do
+            action :upgrade
+        end
+    end
+
+    cookbook_file "/tmp/pyrabbit-1.0.1.tar.gz" do
+        source "pyrabbit-1.0.1.tar.gz"
+        cookbook "bcpc-binary-files"
+        owner "root"
+        mode 00444
+    end
+
+    bash "install-pyrabbit" do
+        code <<-EOH
+            pip install /tmp/pyrabbit-1.0.1.tar.gz
+        EOH
+        not_if "pip freeze|grep pyrabbit"
+    end
+
+    bash "diamond-set-user" do
+        user "root"
+        code <<-EOH
+            sed --in-place '/^DIAMOND_USER=/d' /etc/default/diamond
+            echo 'DIAMOND_USER="root"' >> /etc/default/diamond
+        EOH
+        not_if "grep -e '^DIAMOND_USER=\"root\"' /etc/default/diamond"
+        notifies :restart, "service[diamond]", :delayed
+    end
+
+    template "/etc/diamond/diamond.conf" do
+        source "diamond.conf.erb"
+        owner "diamond"
+        group "root"
+        mode 00600
+        variables(:servers => get_head_nodes)
+        notifies :restart, "service[diamond]", :delayed
+    end
+
+    service "diamond" do
+        action [:enable, :start]
+    end
+end
