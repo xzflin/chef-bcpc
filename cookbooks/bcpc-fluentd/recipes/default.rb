df4d37d054bf0cf411eec475454370feb15417a1
Edmund Rhudy <erhudy@users.noreply.github.com>
2015-08-17 11:19:10 -0400

    bcpc-fluentd converges

--- cookbooks/bcpc-fluentd/recipes/default.rb
+++ cookbooks/bcpc-fluentd/recipes/default.rb
@@ -16,3 +16,74 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+if node['bcpc']['enabled']['logging']
+    apt_repository "fluentd" do
+        uri node['bcpc']['repos']['fluentd']
+        distribution node['lsb']['codename']
+        components ["contrib"]
+        arch "amd64"
+        key "fluentd.key"
+    end
+
+    package "td-agent-v1" do
+        package_name "td-agent"
+        version "1.1.21-1"
+        action :purge
+    end
+
+    bash "clean-up-old-fluentd" do
+        code <<-EOH
+          rm -rf /usr/lib/fluent
+        EOH
+        only_if 'test -d /usr/lib/fluent'
+    end
+
+    # Run td-agent as root
+    cookbook_file "/etc/default/td-agent" do
+        source "td-agent-default"
+        owner "root"
+        mode 00644
+    end
+
+    package "td-agent" do
+        action :upgrade
+    end
+
+    fluentd_gems = %w{
+      excon-0.45.3
+      multi_json-1.11.2
+      multipart-post-2.0.0
+      faraday-0.9.1
+      elasticsearch-transport-1.0.12
+      elasticsearch-api-1.0.12
+      elasticsearch-1.0.12
+      fluent-plugin-elasticsearch-0.9.0
+    }
+
+    fluentd_gems.each do |pkg|
+      cookbook_file "/tmp/#{pkg}.gem" do
+        source "#{pkg}.gem"
+        cookbook "bcpc-binary-files"
+        owner "root"
+        mode 00444
+      end
+
+      bash "install-#{pkg}" do
+        code "/opt/td-agent/embedded/bin/fluent-gem install --local --no-ri --no-rdoc /tmp/#{pkg}.gem"
+        not_if "/opt/td-agent/embedded/bin/fluent-gem list --local --no-versions | grep #{pkg}$"
+      end
+    end
+
+    template "/etc/td-agent/td-agent.conf" do
+        source "fluentd-td-agent.conf.erb"
+        owner "root"
+        group "root"
+        mode 00644
+        notifies :restart, "service[td-agent]", :immediately
+    end
+
+    service "td-agent" do
+        action [:enable, :start]
+    end
+end
