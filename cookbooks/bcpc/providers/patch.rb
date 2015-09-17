21653e83796922ce5b7ddc14c14891ba1a074bc0
Edmund Rhudy <erhudy@users.noreply.github.com>
2015-09-17 18:53:22 -0400

    Adjusted name of stage_patch to stage_file also added parentheses around method calls so I look less arbitrary

--- cookbooks/bcpc/providers/patch.rb
+++ cookbooks/bcpc/providers/patch.rb
@@ -21,18 +21,18 @@ def whyrun_supported?
   true
 end
 
-# expects the name of the patch file and the cookbook (can be nil)
-# returns the path to the staged patch file
-def stage_patch(patch_file, file_cookbook)
-  path = ::File.join Chef::Config[:file_cache_path], patch_file
+# expects the name of the file to stage and the cookbook (can be nil)
+# returns the path to the staged file
+def stage_file(file_to_stage, file_cookbook)
+  path = ::File.join(Chef::Config[:file_cache_path], file_to_stage)
   cookbook = file_cookbook || cookbook_name
 
   Chef::Log.debug("creating #{path}")
   @file_patch_path = Chef::Resource::CookbookFile.new(path, run_context)
-  @file_patch_path.source patch_file
-  @file_patch_path.owner 'root'
-  @file_patch_path.mode 00644
-  @file_patch_path.cookbook cookbook
+  @file_patch_path.source(patch_file)
+  @file_patch_path.owner('root')
+  @file_patch_path.mode(00644)
+  @file_patch_path.cookbook(cookbook)
   @file_patch_path.run_action(:create)
 
   path
@@ -40,9 +40,9 @@ end
 
 action :apply do
   # stage all files
-  before_checksum_path = stage_patch new_resource.shasums_before_apply, new_resource.file_cookbook
-  after_checksum_path = stage_patch new_resource.shasums_after_apply, new_resource.file_cookbook
-  patch_path = stage_patch new_resource.patch_file, new_resource.file_cookbook
+  before_checksum_path = stage_file(new_resource.shasums_before_apply, new_resource.file_cookbook)
+  after_checksum_path = stage_file(new_resource.shasums_after_apply, new_resource.file_cookbook)
+  patch_path = stage_file(new_resource.patch_file, new_resource.file_cookbook)
 
   # test to see if checksums match
   before_cmd_str = "cd #{new_resource.patch_root_dir} && shasum -c #{before_checksum_path}"
@@ -52,11 +52,11 @@ action :apply do
   after_cmd = Mixlib::ShellOut.new(after_cmd_str).run_command
 
   # check stderr on before and after to find out if any files could not be found and raise if so
-  if before_cmd.stderr.end_with? "could not be read\n"
+  if before_cmd.stderr.end_with?("could not be read\n")
     raise "Error reading files during before checksum: stdout: #{before_cmd.stdout}\nstderr:#{before_cmd.stderr}"
   end
 
-  if after_cmd.stderr.end_with? "could not be read\n"
+  if after_cmd.stderr.end_with?("could not be read\n")
     raise "Error reading files during after checksum: stdout: #{after_cmd.stdout}\nstderr:#{after_cmd.stderr}"
   end
 
