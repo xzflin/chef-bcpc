29aa8d3afcc9db8c74d9580c8b993d2100c6a1a3
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2015-10-27 21:01:50 +0000

    Pre-sync trigger

--- cookbooks/bcpc/files/default/sync_pre_unlink_pxe_configs.py
+++ cookbooks/bcpc/files/default/sync_pre_unlink_pxe_configs.py
@@ -1,4 +1,4 @@
-#!/ur/binpyton
+#!/usr/bin/python
 from cobbler import api
 from cobbler import utils
 import os
@@ -13,17 +13,16 @@ def run(api, args, logger):
     """Removes the symlinks. Similar to BootSync.clean_trees()"""
     rc = 0
     tftp_basedir = utils.tftpboot_location()
-    pxecfg_dir = os.path.join(tftp_basedir, 'pxelinux.cfg')
-    if os.path.isdir(pxecfg_dir):
-        try:
-            bootmodes = ['bios', 'efi64']
-            for b in bootmodes:
+    try:
+        bootmodes = ['bios', 'efi64']
+        for b in bootmodes:
+            # logger is uninitialized for some reason at this point
+            if logger is not None:
                 logger.info('Removing PXE configs for %s' % b.upper())
-                pxecfg_path = os.path.join(tftp_basedir, b, 'pxelinux.cfg')
-                if os.path.islink(pxecfg_path):
-                    os.unlink(pxecfg_path)
-        except OSError as err:
-            logger.error(err)
-            rc = err.errno
+            pxecfg_path = os.path.join(tftp_basedir, b, 'pxelinux.cfg')
+            if os.path.islink(pxecfg_path):
+                os.unlink(pxecfg_path)
+    except OSError as err:
+        rc = err.errno
 
     return rc
