c8db6d2860bb6133d4e3e52951a8b7f570ec0b50
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2015-11-03 17:43:29 -0500

    Update haproxy version spec to 1.5.15-1ppa1~trusty

--- cookbooks/bcpc/attributes/default.rb
+++ cookbooks/bcpc/attributes/default.rb
@@ -43,7 +43,7 @@ default['bcpc']['elasticsearch']['version'] = '1.5.1'
 default['bcpc']['ceph']['version'] = '0.94.5-1trusty'
 default['bcpc']['ceph']['version_number'] = '0.94.5'
 default['bcpc']['erlang']['version'] = '1:17.5.3'
-default['bcpc']['haproxy']['version'] = '1.5.14-1ppa~trusty'
+default['bcpc']['haproxy']['version'] = '1.5.15-1ppa1~trusty'
 default['bcpc']['kibana']['version'] = '4.0.2'
 default['bcpc']['rabbitmq']['version'] = '3.5.6-1'
 
