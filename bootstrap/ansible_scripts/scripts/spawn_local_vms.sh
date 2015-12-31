8cff8105cc2ff181d7a510adc0ea6ce391b01f7d
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2015-12-31 11:38:46 -0500

    Add util for offline purging of locally-spawned vms

--- bootstrap/ansible_scripts/scripts/spawn_local_vms.sh
+++ bootstrap/ansible_scripts/scripts/spawn_local_vms.sh
@@ -41,13 +41,7 @@ DMF_SET=$(VBoxManage list systemproperties | grep '^Default machine folder:')
 OLD_DMF_PATH=$(echo "$DMF_SET" | $SED 's/^Default machine folder:[[:space:]]+(.+)$/\1/')
 DMF_PATH="${ANSIBLE_VM_DIR:-$OLD_DMF_PATH}"
 
-# destroy existing ansible BCPC VMs
-candidate_list="ansible-bcpc-bootstrap ansible-bcpc-vm1 ansible-bcpc-vm2 ansible-bcpc-vm3 ansible-bcpc-vm4 ansible-bcpc-vm5 ansible-bcpc-vm6"
-existing_vms=( $(VBoxManage list vms |  awk -v vmpattern="${candidate_list// /|}" '$1 ~ vmpattern {gsub(/"/,"",$1);print $1}') )
-for VM in ${existing_vms[@]}; do
-    VBoxManage controlvm $VM poweroff && true
-    VBoxManage unregistervm $VM --delete
-done
+$REPO_ROOT/bootstrap/ansible_scripts/scripts/vbox_clean.sh
 
 # destroy and recreate host-only networks
 VBoxManage hostonlyif remove vboxnet0 && true
