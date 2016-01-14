8a673c6dafd81bb1fb29d685195ee1814b0e37b7
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2016-01-14 14:49:11 -0500

    Modify clean script

--- bootstrap/vagrant_scripts/vagrant_clean.sh
+++ bootstrap/vagrant_scripts/vagrant_clean.sh
@@ -4,6 +4,14 @@ set -e
 
 . $REPO_ROOT/bootstrap/shared/shared_functions.sh
 
+load_configs
 # Set this to max value to ensure leftover mon VMs are destroyed
 export CLUSTER_NODE_COUNT_OVERRIDE=6
-cd $REPO_ROOT/bootstrap/vagrant_scripts && vagrant destroy -f
+
+cd $REPO_ROOT/bootstrap/vagrant_scripts
+if [[ -n "$MACHINE_NAME_PREFIX" ]]; then
+    vagrant status | awk -v prefix="^${MACHINE_NAME_PREFIX}" \
+        '$1 ~ prefix {print $1}' | xargs vagrant destroy -f
+else
+    vagrant destroy -f
+fi
