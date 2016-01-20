08bc2d68c538b10f12aa986c983123567971661d
Roman Semenov <rsemenov@bloomberg.net>
2016-01-20 13:15:25 -0500

    Add ability to suppress ceph 'pg max obj skew' warning

--- cookbooks/bcpc/attributes/default.rb
+++ cookbooks/bcpc/attributes/default.rb
@@ -102,6 +102,8 @@ default['bcpc']['ceph']['pgp_auto_adjust'] = false
 # Need to review...
 default['bcpc']['ceph']['pgs_per_node'] = 1024
 default['bcpc']['ceph']['max_pgs_per_osd'] = 300
+# Set to 0 to disable. See http://tracker.ceph.com/issues/8103 
+default['bcpc']['ceph']['pg_warn_max_obj_skew'] = 10
 # Journal size could be 10GB or higher in some cases
 default['bcpc']['ceph']['journal_size'] = 2048
 # The 'portion' parameters should add up to ~100 across all pools
