40cc1994b5abfa664643644645d1e465821f45d2
Edmund Rhudy <erhudy@bloomberg.net>
2016-05-23 16:20:53 -0400

    Fixes #1033: fails the Chef run if the host_aggregates provider fails to execute

--- cookbooks/bcpc/providers/host_aggregate.rb
+++ cookbooks/bcpc/providers/host_aggregate.rb
@@ -34,18 +34,10 @@ end
 
 action :create do
   stdout, stderr, status = Open3.capture3(*(openstack_cli +
-  	                                    ["aggregate", "show",
+                                        ["aggregate", "show",
                                              @new_resource.name, "-f", "json" ]))
 
-  if not status.success?
-    converge_by("Creating host aggregate #{new_resource.name}") do
-      args = ["aggregate", "create", @new_resource.name , "-f", "json"]
-      args += ["--zone", "#{@new_resource.zone}"] unless @new_resource.zone.nil?
-      stdout, status = Open3.capture2( *(openstack_cli + args +
-     		                         @new_resource.metadata.collect {|k , v| ["--property", k.to_s + "=" + v.to_s ] }.flatten ))
-      Chef::Log.error "Failed to create host aggregate" unless status.success?
-    end
-  else
+  if status.success?
     ha_fields = JSON.parse(stdout)
     current_properties = ha_fields.select {|x| x['Field'] == "properties"}[0]["Value"]
 
@@ -54,12 +46,20 @@ action :create do
     @new_resource.metadata.each { |k,v| new_properties[k.to_s] = v.to_s }
     if new_properties != current_properties
       converge_by ("Update properties") do
-	args = ["aggregate", "set", @new_resource.name]
-	args += ["--zone", "#{@new_resource.zone}"] unless @new_resource.zone.nil?
-	stdout, status = Open3.capture2( *(openstack_cli +
-    	 		 	           args + new_properties.collect {|k , v| ["--property", k + "=" + v ] }.flatten ))
-	Chef::Log.error "Failed to update host aggregate" unless status.success?
+        args = ["aggregate", "set", @new_resource.name]
+        args += ["--zone", "#{@new_resource.zone}"] unless @new_resource.zone.nil?
+        stdout, stderr, status = Open3.capture3( *(openstack_cli +
+                             args + new_properties.collect {|k , v| ["--property", k + "=" + v ] }.flatten ))
+        raise "Failed to update host aggregate #{@new_resource.name}: #{stderr}" unless status.success?
       end
+    end
+  else
+    converge_by("Creating host aggregate #{new_resource.name}") do
+      args = ["aggregate", "create", @new_resource.name , "-f", "json"]
+      args += ["--zone", "#{@new_resource.zone}"] unless @new_resource.zone.nil?
+      stdout, stderr, status = Open3.capture3( *(openstack_cli + args +
+                                 @new_resource.metadata.collect {|k , v| ["--property", k.to_s + "=" + v.to_s ] }.flatten ))
+      raise "Failed to create host aggregate #{@new_resource.name}: #{stderr}" unless status.success?
     end
   end
 end
@@ -67,15 +67,16 @@ end
 action :member do
   # Adds the current host to the host aggregate
   stdout, stderr, status = Open3.capture3(*(openstack_cli +
-  	                                    ["aggregate", "show", @new_resource.name, "-f", "json" ]))
+                                        ["aggregate", "show", @new_resource.name, "-f", "json" ]))
   raise "Unable to find host aggregate #{@new_resource.name}" unless status.success?
 
   ha_fields = JSON.parse(stdout)
   current_hosts = ha_fields.select {|x| x['Field'] == "hosts"}[0]["Value"]
-  if not current_hosts.include?(node['hostname'])
+  unless current_hosts.include?(node['hostname'])
     converge_by ("Adding host") do
       stdout, stderr, status = Open3.capture3(*(openstack_cli +
-  			                        ["aggregate", "add", "host", @new_resource.name, node['hostname'] ]))
+                                ["aggregate", "add", "host", @new_resource.name, node['hostname'] ]))
+      raise "Failed to add host #{node['hostname']} to aggregate #{@new_resource.name}: #{stderr}" unless status.success?
     end
   end
 end
@@ -83,7 +84,7 @@ end
 action :depart do
   # Removes the current host from the host aggregate
   stdout, stderr, status = Open3.capture3(*(openstack_cli +
-  	                                    ["aggregate", "show", @new_resource.name, "-f", "json" ]))
+                                        ["aggregate", "show", @new_resource.name, "-f", "json" ]))
   raise "Unable to find host aggregate #{@new_resource.name}" unless status.success?
 
   ha_fields = JSON.parse(stdout)
@@ -91,7 +92,8 @@ action :depart do
   if current_hosts.include?(node['hostname'])
     converge_by ("Removing host") do
       stdout, stderr, status = Open3.capture3(*(openstack_cli +
-  			                        ["aggregate", "remove", "host", @new_resource.name, node['hostname'] ]))
+                                ["aggregate", "remove", "host", @new_resource.name, node['hostname'] ]))
+      raise "Failed to remove host #{node['hostname']} from aggregate #{@new_resource.name}: #{stderr}" unless status.success?
     end
   end
 end
