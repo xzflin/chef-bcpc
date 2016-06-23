88d520329b2cf72ce73b05263fe4ec35cd8239a1
Edmund Rhudy <erhudy@bloomberg.net>
2016-06-22 22:08:41 -0400

    Fix Foodcritic complaints about bcpc_common

--- cookbooks/bcpc_common/metadata.rb
+++ cookbooks/bcpc_common/metadata.rb
@@ -5,6 +5,8 @@ license          'Apache License 2.0'
 description      'Installs components common to all BCPC nodes'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '6.0.0'
+issues_url       'https://github.com/bloomberg/chef-bcpc/issues'
+source_url       'https://github.com/bloomberg/chef-bcpc'
 
 depends 'apt', '>= 3.0.0'
 depends 'chef-client', '>= 4.5.0'
