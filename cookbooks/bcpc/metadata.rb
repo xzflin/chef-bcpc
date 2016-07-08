9488e34c92957fddd9a08ed8b5b282fc7a0c1f2b
Edmund Rhudy <erhudy@bloomberg.net>
2016-07-08 15:54:27 -0400

    Update dependent cookbook versions

--- cookbooks/bcpc/metadata.rb
+++ cookbooks/bcpc/metadata.rb
@@ -8,11 +8,12 @@ version          IO.read(File.join(File.dirname(__FILE__), '.version'))
 issues_url       'https://github.com/bloomberg/chef-bcpc/issues'
 source_url       'https://github.com/bloomberg/chef-bcpc'
 
-depends "apt", ">= 1.9.2"
-depends "ubuntu", ">= 1.1.2"
-depends "chef-client", ">= 2.2.2"
-depends "cron", ">= 1.2.2"
-depends "ntp", ">= 1.3.2"
+depends "apt", ">= 3.0.0"
+depends "ubuntu", ">= 1.2.0"
+depends "chef-client", ">= 4.5.0"
+depends "cron", ">= 1.7.6"
+depends "ntp", ">= 2.0.0"
 depends "hostsfile", ">= 2.4.5"
-depends "concat", ">= 0.3.0"
+depends "concat", ">= 0.3.3"
 depends 'bcpc-binary-files', '>= 6.0.0'
+depends 'logrotate', '>= 1.9.2'
