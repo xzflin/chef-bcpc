b6e0250ea5dcb80d465d6e59e2d8af86ea6a2d04
Edmund Rhudy <erhudy@users.noreply.github.com>
2016-09-02 10:06:15 -0400

    Parameterize innodb_buffer_pool_size and innodb_buffer_pool_instances
    ...in all cases, not just pool_size for monitoring

--- cookbooks/bcpc/attributes/monitoring.rb
+++ cookbooks/bcpc/attributes/monitoring.rb
@@ -11,7 +11,8 @@ default['bcpc']['monitoring']['vip'] = "10.17.1.16"
 # List of monitoring clients external to cluster that we are monitoring
 default['bcpc']['monitoring']['external_clients'] = []
 # Monitoring database settings
-default['bcpc']['monitoring']['mysql']['innodb_buffer_pool_size'] = nil
+default['bcpc']['monitoring']['mysql']['innodb_buffer_pool_size'] = '128M'
+default['bcpc']['monitoring']['mysql']['innodb_buffer_pool_instances'] = 1
 # Pagerduty integration
 default['bcpc']['monitoring']['pagerduty']['enabled'] = false
 # Pagerduty service key
