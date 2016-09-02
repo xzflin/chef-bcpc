c72fc4fbea7e7dea8e0bae7fa2bf118cf0a44f00
Edmund Rhudy <erhudy@users.noreply.github.com>
2016-09-02 09:46:10 -0400

    Parameterize Glance debug logging

--- cookbooks/bcpc/attributes/default.rb
+++ cookbooks/bcpc/attributes/default.rb
@@ -402,6 +402,7 @@ default['bcpc']['cinder']['quota'] = {
 ###########################################
 # Verbose logging (level INFO)
 default['bcpc']['glance']['verbose'] = false
+default['bcpc']['glance']['debug'] = false
 default['bcpc']['glance']['workers'] = 5
 
 ###########################################
